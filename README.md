08a74768b440ded93b062d1d45f833135daf922e
Jackson Chandler <jackson.c.chandler@gmail.com>
2024-04-05 18:05:20 -0500

    Updated readme and script

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Mobile Dev Environment
+# Flutter Development Environment
+
+This bash script installs the required packages for Flutter on MacOS as well as update the zshrc shell to use commands.
+
+## Packages installed
+
+- Homebrew
+- VisualStudio Code (optional)
+- Android Studio (optional)
+- git
+- Flutter SDK (which includes the Dart SDK)
+- Cocoapods
+
+If you choose to install Android Studio it will also install the following:
+
+- OpenJDK
+- Android platform tools, ndk, and android-32
+
+It will also accept the licenses and update some packages.
+
+## Caveats
+
+This script is offered as is and may not work perfectly on all machines.
+
+There is also some additional steps that may be required to begin coding in Flutter, such as:
+
+- Installing the Flutter and Dart extensions to VS Code and Android Studio
+- Installing and creating new or additional iOS simulators and Android Emulators
+- Adding the Dart SDK path to Android Studio
+- Installing additional Android version targets in the SDK Manager
+
+For more information on what your system is missing please run:
+> flutter doctor
+
+
+Visit the Flutter getting started guide for additional information.
--- SetupEnvironment.sh
+++ SetupEnvironment.sh
@@ -3,13 +3,13 @@
 while true; do
     read -p "Begin Mobile Environment Setup ...? " yn
     case $yn in
-        [Yy]* ) make install; break;;
+        [Yy]* ) make beginInstallation; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-beginInstallation() {
+function beginInstallation() {
     echo "Beginning Installation"
     
     if ! type "brew" > /dev/null; then
@@ -17,70 +17,61 @@ beginInstallation() {
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 
-    fi ! brew info visual-studio-code &>/dev/null; then
-        echo "Installing Visual Studio Code"
-        brew install --cask visual-studio-code
-    fi
+    while true; do
+        read -p "Do you want to install VisualStudio Code? " yn
+        case $yn in
+            [Yy]* ) installVSCode; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
 
-    fi ! brew info android-studio &>/dev/null; then
-        echo "Installing Android Studio"
-        brew install --cask android-studio
-    fi
+    while true; do
+        read -p "Do you want to install Android Studio? " yn
+        case $yn in
+            [Yy]* ) installAndroidStudio; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
 
     fi ! brew info git &>/dev/null; then
         echo "Installing Git"
         brew install git
     fi
 
+    installFlutterEnv
 }
-
-installFlutterEnv(){
-
-}
-
-installReactNativeEnv(){
-
-    fi ! brew info node &>/dev/null; then
-        echo "Installing Node"
-        brew install node
-    fi
-
-    fi ! brew info watchman &>/dev/null; then
-        echo "Installing Watchman"
-        brew install watchman
-    fi
-
-    fi ! brew info yarn &>/dev/null; then
-        echo "Installing Yarn"
-        brew install yarn
-    fi
-
-    fi ! brew info nvm &>/dev/null; then
-        echo "Installing NVM"
-        brew install nvm
-    fi
-
-    fi ! brew info corepack &>/dev/null; then
-        echo "Installing Corepack"
-        brew install corepack
-    fi
-
-    fi ! brew info npx &>/dev/null; then
-        echo "Installing NPX"
-        brew install npx
-    fi
-
-    package='npx'
-    if [ `npm list -g | grep -c $package` -eq 0 ]; then
-        npm install $package --no-shrinkwrap
+function installVSCode() {
+    fi ! brew info visual-studio-code &>/dev/null; then
+        echo "Installing Visual Studio Code"
+        brew install --cask visual-studio-code
     fi
 }
 
-installAndroidEnv(){
+function installAndroidStudio() {
+    fi ! brew info android-studio &>/dev/null; then
+        echo "Installing Android Studio"
+        brew install --cask android-studio
+    fi
 
-    fi ! brew info adoptopenjdk/openjdk/adoptopenjdk11 &>/dev/null; then
-        echo "Installing AdoptOpenJDK"
-        brew install --cask adoptopenjdk/openjdk/adoptopenjdk11
+    installAndroidEnv
+}
+
+function installFlutterEnv(){
+
+    fi ! brew info flutter &>/dev/null; then
+        echo "Installing Flutter"
+        brew install --cask flutter
+    fi
+}
+
+function installAndroidEnv(){
+
+    fi ! brew info openjdk &>/dev/null; then
+        echo "Installing OpenJDK"
+        brew install --cask openjdk
+        echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
     fi
 
     echo "Updating Android"
@@ -89,8 +80,7 @@ installAndroidEnv(){
         brew install --cask android-sdk
         tools/bin/sdkmanager "platform-tools" "platforms;android-32" "extras;google;m2repository" "extras;android;m2repository"
         tools/bin/sdkmanager --licenses
-        sdkmanager --install "ndk;21.3.6528147" --channel=3
-        sdkmanager --install "cmake;10.24988404"
+        sdkmanager --install "ndk"
         sdkmanager --update
 
     fi
@@ -105,7 +95,7 @@ installAndroidEnv(){
 
 }
 
-installIOSEnv(){
+function installIOSEnv(){
 
     fi ! brew info cocoapods &>/dev/null; then
         echo "Installing CocoaPods"
@@ -115,50 +105,6 @@ installIOSEnv(){
     echo "Updating XCode"
 
     xcode-select --install
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    source ~/.zshrc
-    nvm install v14.15.0
-    nvm use v14.15.0
-    npm i -g corepack
-}
-
-installIonicEnv(){
-
-    fi ! brew info node &>/dev/null; then
-        echo "Installing Node"
-        brew install node
-    fi
-
-    fi ! brew info watchman &>/dev/null; then
-        echo "Installing Watchman"
-        brew install watchman
-    fi
-
-    fi ! brew info yarn &>/dev/null; then
-        echo "Installing Yarn"
-        brew install yarn
-    fi
-
-    fi ! brew info nvm &>/dev/null; then
-        echo "Installing NVM"
-        brew install nvm
-    fi
-
-    fi ! brew info corepack &>/dev/null; then
-        echo "Installing Corepack"
-        brew install corepack
-    fi
-
-    fi ! brew info npx &>/dev/null; then
-        echo "Installing NPX"
-        brew install npx
-    fi
-
-    package='npx'
-    if [ `npm list -g | grep -c $package` -eq 0 ]; then
-        npm install $package --no-shrinkwrap
-    fi
-
 }
 
 
